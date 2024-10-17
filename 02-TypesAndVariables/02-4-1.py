7b4112e85631bc7399c5bf8a514f6b0e04f1d286
Maksym Biliak <maksumbiliak@gmail.com>
2024-10-17 23:30:05 +0200

    done some

--- 02-TypesAndVariables/02-4-1.py
+++ 02-TypesAndVariables/02-4-1.py
@@ -1,6 +1,4 @@
-My name = Adam
+name = "Adam"
 years=22
 height = "300"
-print(f"My name is {name}.
-I am {years} years old, and my height is {height} cm.
-In 6 years I will be {years+6} years old.  ")  
+print(f"My name is {name}. I am {years} years old, and my height is {height} cm. In 6 years I will be {years+6} years old.  ") 
---
+++ 02-TypesAndVariables/02-5-3.py
@@ -0,0 +1,10 @@
+###
+# A program that calculates the volume
+# and surface area of ​​a cuboid with sides a, b, and c.
+# Read the dimensions of the cuboid from the keyboard.
+#
+a = input('a=')
+b = input('b=')
+c = input('c=')
+volume = a*b*c
+print (volume)
