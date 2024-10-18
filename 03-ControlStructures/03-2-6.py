2111490f9a0f2a919656d12587c1a94834a6be13
Maksym Biliak <maksumbiliak@gmail.com>
2024-10-18 10:18:57 +0200

    chinazess

---
+++ 03-ControlStructures/03-2-6.py
@@ -0,0 +1,7 @@
+a  =int(input())
+if a>0:
+    print ("pos")
+elif a==0:
+    print ("zero")
+else:
+    print('negative')
---
+++ 03-ControlStructures/03-3-6.py
@@ -0,0 +1,18 @@
+###
+# Checks if the given day number of the month is correct
+#
+month = int(input('Enter month number (1..12): '))
+day = int(input('Enter the day number of the month: '))
+day_ok = False
+
+if month==1 or month==3 or ...:
+    if day >=1 and day <= 31:
+        day_ok = True
+elif month== ...:
+    if day >=1 and day <= ...:
+        ...
+...
+
+message = f'Day {...} for the month {...}'
+if day_ok:
+    print('{message} is correct')
